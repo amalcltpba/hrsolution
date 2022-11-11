41c7bc635fc720c3f8b28bd0042687b1bc7a4fcc
Amal <amalscreations04@gmail.com>
2022-11-11 11:41:36 +0530

    Login page integrated
    ogin component created.
    PrimeNG Login input and button integrated.
    Login navigation added.
    The config changed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ButtonModule } from 'primeng/button';
+import { CheckboxModule } from 'primeng/checkbox';
+import { InputTextModule } from 'primeng/inputtext';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './module/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule,
+    CheckboxModule,
+    InputTextModule
   ],
   providers: [],
   bootstrap: [AppComponent]
